0f674111bc9a4e0509cfa626627914b05a682a90
Ruben Novelli <rubendario.novel@gmail.com>
2019-05-20 15:58:30 -0300

    prueba heroku

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const compression = require('compression');
+//Install express server
 const express = require('express');
+const compression = require('compression');
+const path = require('path');
 
 const app = express();
 app.use(compression());
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist/desapp-groupL-frontend'));
 
 app.get('/*', function(req,res) {
-    res.sendFile('index.html', { root: __dirname });
+
+    res.sendFile(path.join(__dirname+'/dist/desapp-groupL-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
+
+
+
